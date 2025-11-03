1a3ec0fc01f130a2ca5c411fcd37892fd2ab9d74
Kevin MacDonald <iamkevinmac@gmail.com>
2025-11-03 11:58:28 -0800

    created jwt auth system

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 import express from "express";
+import getUserFromToken from "#utils/getUserFromToken.js";
+import usersRouter from "#routers/users.js";
+
 const app = express();
+
+//MIDDLEWARE (order matters) --> parse body, extract user, route, catch errors
+//parse JSON req body first -> so it is available to routes that need req.body
+app.use(express.json());
+
+//extract user from token
+app.use(getUserFromToken);
+
+//mount users router @ /users (where I have register & login POST routes)
+app.use("/users", usersRouter);
+
+//catch all error handler (must come last)
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Internal server error");
+});
+
 export default app;
